de1d892a03c1f23c0b86c3bbb2a04eef0d19bcb4
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-05-23 22:04:37 +1200

    Mouse working, beginning work on cat

--- script.js
+++ script.js
@@ -1,74 +1,114 @@
 // Starting sequence
-document.addEventListener("keydown",startSeq);
-
-function startSeq(e){
-	if(e.which==32){
-		document.getElementById("start").innerHTML = "Go!";		
-		document.removeEventListener("keydown",startSeq);
-		document.addEventListener("keyup",aUp);
-		document.addEventListener("keyup",lUp);
+// Randomly place cat and mouse on Y axis
+function startPosns() {
+	var icons = ["cat","mouse"];
+	var top = [];
+	var iconHeight;
+	var trackHeight = document.getElementById("track").clientHeight;
+	for (var i = 0; i < icons.length; i++) {
+		iconHeight = document.getElementById(icons[i]).clientHeight;
+		top[i] = (trackHeight - iconHeight) * Math.random();
 	}
+	// If too close, call function again
+	if (Math.abs(top[0] - top[1]) < 75) {
+		top = [];
+		startPosns();
+	}
+	// Render cat and mouse at starting positions
+	$("#cat").css("top", top[0]);
+	$("#mouse").css("top", top[1]);	
 }
 
-var count1 = 2;
-var count2 = 2;
-var str1 = count1.toString();
-var str2 = count2.toString();
+// Mouse movement
+function moveMouse() {
+	// Move right
+	var xPos = $("#mouse").position().left;
+	var xMove = $("#track").width() / 50;
+	$("#mouse").css("left", xPos + xMove);
 
-// Move car 1
-function aUp(e){
-	if(count1>87){
-		// Display player 1 wins and remove key listeners
-		gameOver();
-		document.getElementById("winLose1").innerHTML = "WINNER!";
-		document.getElementById("winLose2").innerHTML = "LOSER!";
+	// Choose if mouse moves up or down and move accordingly
+	var yPos = $("#mouse").position().top;
+	var yMove = $("#track").height() / 50;
+
+	// Move up
+	if (Math.random() > 0.5) {
+		if( yPos - yMove > 0) {
+			$("#mouse").css("top", yPos - yMove);
+		}
+		else {
+			$("#mouse").css("top",0);
+		}
 	}
-	else if(e.which==65){
-		count1 +=1;
-		str1 = count1.toString();
-		document.getElementById("car1").style.left = str1 + "%";
+	// Move down
+	else {
+		var maxTop = $("#track").height() - $("#mouse").height();
+		if ( yPos + yMove < maxTop ) {
+			//move it down
+			$("#mouse").css("top", yPos + yMove);
+		}
+		else {
+			$("#mouse").css("top", $("#track").height() - $("#mouse").height());
+		}
 	}
+
+	// isGameOver();
 }
 
-// Move car 2
-function lUp(e){
-	if(count2>87){
-		// Display player 2 wins and remove key listeners
-		gameOver();
-		document.getElementById("winLose1").innerHTML = "LOSER!";
-		document.getElementById("winLose2").innerHTML = "WINNER!";
+
+// Cat movement
+function moveCat(e) {
+	var xPos = $("#cat").position().left;
+	var tWidth = $("#track").width(); 
+	var xMove = tWidth / 50;
+	console.log(xPos);
+
+
+// FIX ME UP FROM HERE
+// MAKE IT SO IF CAT MOVES TOO FAR RIGHT, SET ITS POSN RIGHT MOST IN TRACK
+	// Right arrow
+	if (e.which === 39) {
+		if ( xPos + xMove > $("#") )
+		$("#cat").css("left", xPos + xMove);
+		console.log(xPos);
 	}
-	else if(e.which==76){
-		count2 +=1;
-		str2 = count2.toString();
-		document.getElementById("car2").style.left = str2 + "%";
+
+	// Left arrow
+	if (e.which === 37) {
+		if( xPos - xMove < 0 ) {
+			$("#cat").css("left", 0);
+			console.log("Can't move back");
+		}
+		else {
+			$("#cat").css("left", xPos - xMove);
+		}
+		console.log(xPos);
 	}
+
+	// Up arrow
+	// if (e.which === 38) {
+
+	// }
+
+
 }
 
-// Ending sequence
-function gameOver(){
-	document.removeEventListener("keyup",aUp);
-	document.removeEventListener("keyup",lUp);
-	document.getElementById("winLose1").style.display = "block";
-	document.getElementById("winLose2").style.display = "block";
-	document.getElementById("car1").style.display = "none";
-	document.getElementById("car2").style.display = "none";
-	document.getElementById("playAgain").style.display = "block";
-	var instructions = document.getElementsByClassName("instructions");
-	instructions[0].style.opacity = 0;
-	instructions[1].style.opacity = 0;
-	document.addEventListener("keyup",trump);
-}
 
-// Reload page when race has finished and press space
-function trump(e){
-	if(e.which==32){
-		document.getElementById("wrapper").style.display = "none";
-		document.body.style.cssText = 'background-image: url("images/trump2.jpg"); background-size: cover;';
-		window.setTimeout(reset, 250);
-	}
-}
 
-function reset(){
-	location.reload();
-}
+
+// Run
+// --- RUN ---
+$(function () {
+	// Add Event Listeners
+	document.addEventListener("keydown",moveCat);
+
+	startPosns();
+	window.setInterval(function() {
+		moveMouse();
+	}, 5000);
+
+
+})
+
+
+
+
